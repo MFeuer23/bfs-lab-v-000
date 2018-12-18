ddd6e493fe4b7c75404d358682e183eec530883f
Mallory Feuer <mallory.feuer@gmail.com>
2018-12-18 13:48:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function bfs(rootNode, vertices, edges){
+  let queue = [];
+  rootNode = vertices[0];
+  
+}
 
+function findAdjacent(firstNode, vertices, edges) {
+  return vertices.filter(x => x.name == `${firstNode}`)
+}
+
+function markDistanceAndPredecessor(vertex) {
+  
+}
+
+function addToQueue(vertex) {
+  
 }
