c0c63dc3a265e3fa043ba05fdee05d2bdb277f00
Mallory Feuer <mallory.feuer@gmail.com>
2018-12-18 14:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function bfs(rootNode, vertices, edges){
 }
 
 function findAdjacent(firstNode, vertices, edges) {
+  let verticesArray = []
   let adjEdge = edges.filter(x => x.includes(`${firstNode}`))
-  let adjArray =adjEdge.map(x => x.find(function(element) {return element != firstNode}))
+  let adjArray = adjEdge.map(x => x.find(function(element) {return element != firstNode}))
   return vertices.filter(x => x.name == `${firstNode}`)
 }
 
