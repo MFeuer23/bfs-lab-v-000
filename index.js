79641446260db44ea2a00971ea6fb44b621f109d
Mallory Feuer <mallory.feuer@gmail.com>
2018-12-18 14:22:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function bfs(rootNode, vertices, edges){
 function findAdjacent(firstNode, vertices, edges) {
   let adjEdge = edges.filter(x => x.includes(`${firstNode}`))
   let adjArray = adjEdge.map(x => x.find(function(element) {return element != firstNode}))
-  return vertices.filter(x => adjArray.includes(x.name))
+  return vertices.filter(x => adjArray.includes(x.name) && !x.distance)
 }
 
 function markDistanceAndPredecessor(vertex) {
